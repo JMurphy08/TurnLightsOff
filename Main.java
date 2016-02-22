99583d11b2b771045bc8a1df9d27ae98e081e948
JMurphy08 <jeremyrmurphy8@gmail.com>
2016-02-22 19:26:02 -0400

    FlipSwitch
    Added method to switch a lightswitch

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 /***
 Turn the Lights Off #10309
 Jonathan Murray - 3439348 
-Jeremy Murphy - XXXXXXX
+Jeremy Murphy - 3443327
 ***/
 
 import java.util.*;
@@ -29,7 +29,16 @@ class Main {
 					grid[r][c] = String.valueOf(line.charAt(c));
 				}
 			}
-			System.out.println(name + " " + turnLightsOff(grid));
+
+			String[][] newGrid = flipSwitch(grid, 5,5);
+			for(int i=0;i<10;i++){
+				for(int j=0; j<10; j++){
+					System.out.print(newGrid[i][j] + " ");
+				}
+				System.out.println("");
+			}
+			System.out.println("");
+			//System.out.println(name + " " + turnLightsOff(grid));
 		}
 	}
 
@@ -40,10 +49,60 @@ class Main {
 	{
 		int result = 0;
 
-		//TODO
-
+		
 
 		return result;
+	}
+
+	static String[][] flipSwitch(String[][] grid, int r, int c){
+
+		//checkMiddle
+		if(grid[r][c].equals("O")){
+				grid[r][c] = "#";
+		}
+		else{
+			grid[r][c] = "O";
+		}
+
+
+		//check down
+		if(r+1 < 10){
+			if(grid[r+1][c].equals("O")){
+				grid[r+1][c] = "#";
+			}
+			else{
+				grid[r+1][c] = "O";
+			}
+		} 
+		//check up
+		if(r-1 >= 0){
+			if(grid[r-1][c].equals("O")){
+				grid[r-1][c] = "#";
+			}
+			else{
+				grid[r-1][c] = "O";
+			}
+		} 
+		//check right
+		if(c+1 < 10){
+			if(grid[r][c+1].equals("O")){
+				grid[r][c+1] = "#";
+			}
+			else{
+				grid[r][c+1] = "O";
+			}
+		} 
+		//check left
+		if(c-1 >=0){
+			if(grid[r][c-1].equals("O")){
+				grid[r][c-1] = "#";
+			}
+			else{
+				grid[r][c-1] = "O";
+			}
+		} 
+
+		return grid;
 	}
 
 }
